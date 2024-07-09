3dc335bdcdbf86656a3979a0af44f811274ca202
Wambui Ndungu <wambuin18@gmail.com>
2024-07-09 15:25:42 +0300

    make home page responsive

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,23 @@ import Link from "next/link";
 
 export default function page() {
   return (
-    <main className="h-full grid grid-cols-7 gap-[1rem]">
-      <div className="bg-[url('/Hero.png')] bg-cover bg-no-repeat bg-bottom rounded-xl col-span-5 h-full">
+    <main className="h-full grid lg:grid-cols-7 lg:grid-rows-1 grid-cols-1 md:grid-rows-7 grid-row-5 gap-[1rem]">
+      <div className="bg-[url('/Hero.png')] bg-cover bg-no-repeat bg-bottom rounded-xl lg:col-span-5 md:row-span-5 row-span-2 h-full">
         <div className="w-full bg-black/30 p-[2rem] h-full flex flex-col justify-end rounded-xl">
           <div>
             <h1
-              className="uppercase text-[150px] leading-none"
+              className="uppercase lg:text-9xl md:text-7xl text-4xl leading-none"
               style={{ fontFamily: "myFont" }}
             >
               Yamas
             </h1>
-            <p className="uppercase text-5xl" style={{ fontFamily: "myFont" }}>
+            <p className="uppercase lg:text-5xl md:text-4xl text-2xl" style={{ fontFamily: "myFont" }}>
               Sip, Savor, Celebrate.
             </p>
           </div>
         </div>
       </div>
-      <div className="col-span-2 grid grid-rows-3 gap-4">
+      <div className="h-full lg:col-span-2 md:row-span-2 row-span-3 flex lg:flex-col md:flex-row flex-col gap-4">
         <div className="bg-[url('/Menu.png')] bg-cover bg-center rounded-xl w-full h-full flex items-end justify-end">
           <Link href="/menu" className="bg-black px-6 py-2 rounded-tl-lg uppercase text-sm">
             <p>
