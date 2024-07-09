e57a2bfaad564f629a6315f9822cb73b1bc751b0
Wambui Ndungu <wambuin18@gmail.com>
2024-07-09 22:57:08 +0300

    adjust ui

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default async function page() {
   await new Promise((resolve) => {
-    setTimeout(resolve, 5000);
+    setTimeout(resolve, 1000);
   });
   return (
     <main className="h-full grid lg:grid-cols-7 lg:grid-rows-1 grid-cols-1 md:grid-rows-7 grid-row-5 gap-[1rem]">
@@ -25,30 +25,123 @@ export default async function page() {
         </div>
       </div>
       <div className="h-full lg:col-span-2 lg:row-span-1 md:row-span-2 row-span-3 flex lg:flex-col md:flex-row flex-col gap-4">
-        <div className="bg-[url('/Menu.png')] bg-cover bg-center rounded-xl w-full h-full flex items-end justify-end">
+        <div className="relative bg-[url('/Menu.png')] bg-cover bg-center rounded-xl w-full h-full flex items-end justify-end">
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M25.25 26.25H4.40914C8.02024 25.5326 12.1864 23.983 15.876 21.1867C20.249 17.8725 23.9419 12.8142 25.25 5.34475V26.25Z"
+              fill="#090000"
+              stroke="#090000"
+              stroke-width="0"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <svg
+            className="absolute bottom-[36px]"
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M25.25 26.25H4.40914C8.02024 25.5326 12.1864 23.983 15.876 21.1867C20.249 17.8725 23.9419 12.8142 25.25 5.34475V26.25Z"
+              fill="#090000"
+              stroke="#090000"
+              stroke-width="0"
+              stroke-linejoin="round"
+            />
+          </svg>
           <Link
             href="/menu"
-            className="bg-black px-6 py-2 rounded-tl-lg uppercase text-sm"
+            className="bg-black px-6 py-2 rounded-tl-2xl uppercase text-sm"
           >
             <p style={{ fontFamily: "myFont" }} className="tracking-wide">
               Menu <span className="w-2 h-2">&#129122;</span>
             </p>
           </Link>
         </div>
-        <div className="bg-[url('/Reservation.png')] bg-cover bg-center rounded-xl w-full h-full flex items-end justify-end">
+        <div className="relative bg-[url('/Reservation.png')] bg-cover bg-center rounded-xl w-full h-full flex items-end justify-end">
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M25.25 26.25H4.40914C8.02024 25.5326 12.1864 23.983 15.876 21.1867C20.249 17.8725 23.9419 12.8142 25.25 5.34475V26.25Z"
+              fill="#090000"
+              stroke="#090000"
+              stroke-width="0"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <svg
+            className="absolute bottom-[36px]"
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M25.25 26.25H4.40914C8.02024 25.5326 12.1864 23.983 15.876 21.1867C20.249 17.8725 23.9419 12.8142 25.25 5.34475V26.25Z"
+              fill="#090000"
+              stroke="#090000"
+              stroke-width="0"
+              stroke-linejoin="round"
+            />
+          </svg>
           <Link
             href="/reservation"
-            className="bg-black px-6 py-2 rounded-tl-lg uppercase text-sm"
+            className="bg-black px-6 py-2 rounded-tl-2xl uppercase text-sm"
           >
             <p style={{ fontFamily: "myFont" }} className="tracking-wide">
               Reservation <span className="w-2 h-2">&#129122;</span>
             </p>
           </Link>
         </div>
-        <div className="bg-[url('/Gift_Card.png')] bg-cover bg-center rounded-xl w-full h-full flex items-end justify-end">
+        <div className="relative bg-[url('/Gift_Card.png')] bg-cover bg-center rounded-xl w-full h-full flex items-end justify-end">
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M25.25 26.25H4.40914C8.02024 25.5326 12.1864 23.983 15.876 21.1867C20.249 17.8725 23.9419 12.8142 25.25 5.34475V26.25Z"
+              fill="#090000"
+              stroke="#090000"
+              stroke-width="0"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <svg
+            className="absolute bottom-[36px]"
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M25.25 26.25H4.40914C8.02024 25.5326 12.1864 23.983 15.876 21.1867C20.249 17.8725 23.9419 12.8142 25.25 5.34475V26.25Z"
+              fill="#090000"
+              stroke="#090000"
+              stroke-width="0"
+              stroke-linejoin="round"
+            />
+          </svg>
           <Link
             href="/contact"
-            className="bg-black px-6 py-2 rounded-tl-lg uppercase text-sm"
+            className="bg-black px-6 py-2 rounded-tl-2xl uppercase text-sm"
           >
             <p style={{ fontFamily: "myFont" }} className="tracking-wide">
               Contact <span className="w-2 h-2">&#129122;</span>
